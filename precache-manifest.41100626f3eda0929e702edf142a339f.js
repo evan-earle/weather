2c39dcbfd89de6b72cafbd02e53f0f59edc22041
Ozark19 <55114864+Ozark19@users.noreply.github.com>
2019-12-30 23:37:04 -0500

    Updates

---
+++ precache-manifest.41100626f3eda0929e702edf142a339f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc4073abdbc2fdee78fa27c1d9ff47b",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "f6f18573b3b0b825a983",
+    "url": "/weather/static/css/2.cd8266bd.chunk.css"
+  },
+  {
+    "revision": "4dd670dc6e25c3a907f8",
+    "url": "/weather/static/css/main.786b7ce3.chunk.css"
+  },
+  {
+    "revision": "f6f18573b3b0b825a983",
+    "url": "/weather/static/js/2.07af4ee8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather/static/js/2.07af4ee8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4dd670dc6e25c3a907f8",
+    "url": "/weather/static/js/main.b9c82d48.chunk.js"
+  },
+  {
+    "revision": "6ea7e77960c1989f63b6",
+    "url": "/weather/static/js/runtime-main.f857f3f3.js"
+  },
+  {
+    "revision": "6bdfa5dad90c8a1d38bbb692972cd22a",
+    "url": "/weather/static/media/clearday.6bdfa5da.jpg"
+  },
+  {
+    "revision": "69fd96fda561ef0476ba682024758778",
+    "url": "/weather/static/media/clearnight.69fd96fd.JPG"
+  },
+  {
+    "revision": "091b8dfb804bb172b38d204f74c84eb9",
+    "url": "/weather/static/media/cloudday.091b8dfb.JPG"
+  },
+  {
+    "revision": "28ef48a2fc6ae8f5bfb711f0170cd376",
+    "url": "/weather/static/media/cloudnight.28ef48a2.JPG"
+  },
+  {
+    "revision": "ae4b352b03f03b9cfbef62c81d5afa54",
+    "url": "/weather/static/media/drizzleday.ae4b352b.JPG"
+  },
+  {
+    "revision": "702eba1d504907d322e0e3c559a8f669",
+    "url": "/weather/static/media/drizzlenight.702eba1d.JPG"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/weather/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/weather/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/weather/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/weather/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/weather/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "f92070c641da02cc8686578a9c3327f2",
+    "url": "/weather/static/media/rainday.f92070c6.JPG"
+  },
+  {
+    "revision": "03654807ad5cac39f8a7563e1e7f1ccb",
+    "url": "/weather/static/media/rainnight.03654807.JPG"
+  },
+  {
+    "revision": "662cf103d84f1021a29d85ae79b69e9e",
+    "url": "/weather/static/media/snowday.662cf103.JPG"
+  },
+  {
+    "revision": "5e1a0f63b3473add383b15611c09cc12",
+    "url": "/weather/static/media/snownight.5e1a0f63.JPG"
+  },
+  {
+    "revision": "eeb751343df6a6e3f3544adbbd0b2365",
+    "url": "/weather/static/media/thunder.eeb75134.JPG"
+  }
+]);
